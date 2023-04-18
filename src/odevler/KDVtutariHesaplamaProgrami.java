f544834757ce842778da69ffb9592c1adc022097
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-18 19:04:12 +0300

    18.04.2023

--- src/odevler/KDVtutariHesaplamaProgrami.java
+++ src/odevler/KDVtutariHesaplamaProgrami.java
@@ -13,7 +13,14 @@ public class KDVtutariHesaplamaProgrami {
 
         if (fiyat<=1000){
             float tmp=fiyat;
+            fiyat*=1.18;
             System.out.println("KDV orani: %18");
+            System.out.println("KDV tutari: "+(fiyat-tmp));
+            System.out.println("KDV'li fiyat: "+fiyat);
+        }else{
+            float tmp=fiyat;
+            fiyat*=1.08;
+            System.out.println("KDV orani: %8");
             System.out.println("KDV tutari: "+(fiyat-tmp));
             System.out.println("KDV'li fiyat: "+fiyat);
         }
